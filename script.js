9dff7384019214137a29ea9e5581ce96f403db11
swanapole <swanapoleshilla@yahoo.com>
2018-11-29 19:37:22 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var triangle = function(){
+  var values = [document.getElementById](firstValue).value, document.getElementById("secondValue").value, document.getElementById("thirdValue")
+};
+var lengthA = parseInt(sides[0]);
+var lengthB = parseInt(sides[1]);
+var lengthC = parseInt(sides[2]);
+
+isTriangle(firstValue, secondValue, thirdValue){
+  if(firstValue == secondValue|| firstValue == thirdValue || thirdValue == secondValue){
+    window.alert("This is an isosceles triangle");
+  }else if(firstValue == secondValue && secondValue == thirdValue){
+    window.alert("this is an equilateral triangle")
+ }else(firstValue!==secondValue! && secondValue!==thirdValue!){
+  window.alert("This is a scalene")
+ }
+};
