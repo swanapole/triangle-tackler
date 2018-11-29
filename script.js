d2fb1034b02333097ad056a87439c5b5e33c6c4d
swanapole <swanapoleshilla@yahoo.com>
2018-11-29 20:09:05 +0300

    add all the changes

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 
-var triangle = function(){
-  var values = [document.getElementById](firstValue).value, document.getElementById("secondValue").value, document.getElementById("thirdValue")
-};
-var lengthA = parseInt(sides[0]);
-var lengthB = parseInt(sides[1]);
-var lengthC = parseInt(sides[2]);
+function triangleTracker(){
 
-isTriangle(firstValue, secondValue, thirdValue){
-  if(firstValue == secondValue|| firstValue == thirdValue || thirdValue == secondValue){
-  alert("This is an isosceles triangle");
-  }else if(firstValue == secondValue && secondValue == thirdValue){
-    alert("this is an equilateral triangle");
- }else(firstValue!==secondValue! && secondValue!==thirdValue!){
-  alert("This is a scalene");
- }
-};
+       var a = parseInt(document.getElementById("a").value);
+       var b = parseInt(document.getElementById("b").value);
+       var c = parseInt(document.getElementById("b").value);
+
+           if (a ===b && b === c && c === a) {
+             alert("Equilateral Triangle");
+         }
+           else if (a === b || a === c || b === c ) {
+             alert("Isosceles Triangle");
+         }
+           else if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
+             alert("THIS IS NOT A Triangle");
+         }
+           else if (a !== b && b !== c ) {
+             alert("Scalene Triangle");
+         }
+       }
